c90d41a4e0ab6516d8db42e31658af022df0d30c
Quadrapods <82327127+Quadrapods@users.noreply.github.com>
2022-05-16 18:06:22 -0400

    Update Get-Closest-Constant.lua

--- Get-Closest-Constant.lua
+++ Get-Closest-Constant.lua
@@ -1,4 +1,4 @@
-return function(x)
+return function(x, y)
     local a = game:GetService('Players').LocalPlayer;
     local b = debug.getconstants;
     local c = getscriptclosure;
@@ -6,8 +6,21 @@ return function(x)
 
     for _, v in ipairs(a:GetDescendants()) do
         if (v and v:IsA('LocalScript') and #b(c(v)) >= tonumber(x)) then
-            d = v;
+            if (y) then
+                if (not table.find(y, v.Name)) then
+                    d = v;
+                    continue;
+                end
+            else
+                d = v;
+                continue;
+            end
         end
     end
+
+    if (d == nil) then
+        error(string.format('No LocalScripts found with >= %s constants!', tostring(x)));
+    end
+
     return (d)
 end
