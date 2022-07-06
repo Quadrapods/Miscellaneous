88863e99a174a8273e299c8e0c5dfcade0fc3936
Quadrapods <82327127+Quadrapods@users.noreply.github.com>
2022-07-05 23:58:40 -0400

    Update Mouse-Silent-Aim.lua

--- Mouse-Silent-Aim.lua
+++ Mouse-Silent-Aim.lua
@@ -1,27 +1,46 @@
 ---------------------------------------------------------------------------------------------------- // Dependencies
-local GetTarget = loadstring(game:HttpGetAsync('https://raw.githubusercontent.com/Quadrapods/Miscellaneous/main/Get-Closest-Player.lua'))()
+local GetTarget = loadstring(syn.request({Url = 'https://raw.githubusercontent.com/Quadrapods/Miscellaneous/main/Get-Closest-Player.lua', Method = 'GET'}).Body)();
 
 ---------------------------------------------------------------------------------------------------- // Declarations
-local a = game:GetService('Players').LocalPlayer
-local b = game:GetService('UserInputService')
-local c = game:GetService('HttpService')
-local d = game:GetService('RunService')
-local e = game:GetService('Workspace')
-local f = game:GetService('Players')
+local a = game:GetService('Players').LocalPlayer;
+local b = game:GetService('UserInputService');
+local c = game:GetService('HttpService');
+local d = game:GetService('RunService');
+local e = game:GetService('Workspace');
 
-local g = a:GetMouse()
-local h
+local f = a:GetMouse();
+local g;
 
 ---------------------------------------------------------------------------------------------------- // Functions
-h = hookmetamethod(a, '__index', function(...)
-    local x = {...}
+local function GetCharacter()
+    local a = GetTarget(500);
 
-    if (not checkcaller() and rawequal(x[2], 'Hit')) then
-        local y = GetTarget(500)
+    if (a) then
+        local b = a.Character;
 
-        if (y) then
-            return y.Character:WaitForChild('HumanoidRootPart').CFrame
+        if (b) then
+            local c = b:FindFirstChild('HumanoidRootPart');
+
+            if (c) then
+                return (c.CFrame);
+            end
         end
     end
-    return h(...)
+end
+
+---------------------------------------------------------------------------------------------------- // Runtime
+g = hookmetamethod(game, '__index', function(...)
+    local x = {...};
+
+    if (not checkcaller()) then
+        if (rawequal(x[1], f) and rawequal(x[2], 'Hit')) then
+            local y = GetCharacter();
+
+            if (y) then
+                return (y);
+            end
+        end
+    end
+
+    return g(...);
 end)
